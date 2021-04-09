71374b76ef98be644d20c7ddf202fe6367de8444
Ruben Matamoros <ruben@valiantcreative.net>
2021-04-09 00:55:52 -0400

    Created Success Component to Save Order in the Database and Finalized the App

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -13,6 +13,7 @@ const CartItem = ({ item }) => {
       _id: item._id
     });
     idbPromise('cart', 'delete', { ...item });
+
   };
 
   const onChange = (e) => {
@@ -22,16 +23,16 @@ const CartItem = ({ item }) => {
         type: REMOVE_FROM_CART,
         _id: item._id
       });
-    
       idbPromise('cart', 'delete', { ...item });
+
     } else {
       dispatch({
         type: UPDATE_CART_QUANTITY,
         _id: item._id,
         purchaseQuantity: parseInt(value)
       });
-    
       idbPromise('cart', 'put', { ...item, purchaseQuantity: parseInt(value) });
+
     }
   }
 
--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,12 +1,11 @@
 import React, { useEffect } from "react";
-import { useQuery } from '@apollo/react-hooks';
-
 import ProductItem from "../ProductItem";
 import { useStoreContext } from "../../utils/GlobalState";
 import { UPDATE_PRODUCTS } from "../../utils/actions";
+import { useQuery } from '@apollo/react-hooks';
 import { QUERY_PRODUCTS } from "../../utils/queries";
-import spinner from "../../assets/spinner.gif";
 import { idbPromise } from "../../utils/helpers";
+import spinner from "../../assets/spinner.gif"
 
 function ProductList() {
   const [state, dispatch] = useStoreContext();
@@ -18,22 +17,18 @@ function ProductList() {
   useEffect(() => {
     if(data) {
       dispatch({
-        type: UPDATE_PRODUCTS,
-        products: data.products
-      });
-  
-      data.products.forEach((product) => {
-        idbPromise('products', 'put', product);
-      });
-      // add else if to check if `loading` is undefined in `useQuery()` Hook
+           type: UPDATE_PRODUCTS,
+          products: data.products
+        });
+        data.products.forEach((product) => {
+          idbPromise('products', 'put', product);
+        });
     } else if (!loading) {
-      // since we're offline, get all of the data from the `products` store
       idbPromise('products', 'get').then((products) => {
-        // use retrieved data to set global state for offline browsing
         dispatch({
           type: UPDATE_PRODUCTS,
-          products: products
-        });
+         products: products
+       });
       });
     }
   }, [data, loading, dispatch]);
